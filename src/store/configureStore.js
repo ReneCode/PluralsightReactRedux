311cd2c15962d202e04e03e9b4b30b2b1c38e68e
HarryMiller <HarryMillerMail@googlemail.com>
2016-07-22 08:16:08 +0200

    production added

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,12 +1,13 @@
-import {createStore, applyMiddleware} from 'redux';
-import rootReducer from '../reducers';
-import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
-import thunk from 'redux-thunk';
 
-export default function configureStore(initialState) {
-    return createStore(
-        rootReducer,
-        initialState,
-        applyMiddleware(thunk, reduxImmutableStateInvariant())
-    );
+// import * from ....  does not work
+// dynamicly
+// so use require()
+
+// no default export 
+// eslint defaulting in src/index.js
+
+if (process.env.NODE_ENV == 'production') {
+    module.exports = require('./configureStore.prod');
+} else {
+    module.exports = require('./configureStore.dev');
 }
